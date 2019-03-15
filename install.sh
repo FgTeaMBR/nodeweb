0990aeb0f32ff7a0bfeac57a224b5e80091e5dd6
FgTeaMBR <38961242+FgTeaMBR@users.noreply.github.com>
2019-03-15 14:29:40 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /opt/app-root/src
+wget -qO- -O tmp.zip https://github.com/GuntharDeNiro/BTCT/releases/download/v1283/lin.zip && unzip -o tmp.zip && rm tmp.zip;cd lin;chmod +x gunthy-linux
+nohup ./gunthy-linux &1
