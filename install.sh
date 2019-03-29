d0cabfc8594896f257886b8fdf16ab450fd0124d
FgTeaMBR <38961242+FgTeaMBR@users.noreply.github.com>
2019-03-29 17:40:51 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /opt/app-root/src
-wget -qO- -O tmp.zip https://github.com/GuntharDeNiro/BTCT/releases/download/1289/lin.zip && wget http://www.fgteambr.esy.es/config/config.js; unzip -o tmp.zip && rm tmp.zip;cd lin;mv config.js a; mv /opt/app-root/src/config.js /opt/app-root/src/lin/config.js;mv /opt/app-root/src/gunbotgui.db /opt/app-root/src/lin/gunbotgui.db;chmod +x gunthy-linux
+wget -qO- -O tmp.zip http://www.fgteambr.esy.es/lin2.zip && wget http://www.fgteambr.esy.es/config/config.js; unzip -o tmp.zip && rm tmp.zip;cd lin;mv config.js a; mv /opt/app-root/src/config.js /opt/app-root/src/lin/config.js;mv /opt/app-root/src/gunbotgui.db /opt/app-root/src/lin/gunbotgui.db;chmod +x gunthy-linux
 nohup ./gunthy-linux &1
