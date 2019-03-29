8b6f3d4db27ecb97e103146565deda2b841d8b84
FgTeaMBR <38961242+FgTeaMBR@users.noreply.github.com>
2019-03-29 17:56:14 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /opt/app-root/src
-wget -qO- -O tmp.zip http://www.fgteambr.esy.es/lin2.zip && wget http://www.fgteambr.esy.es/config/config.js; unzip -o tmp.zip && rm tmp.zip;cd lin;mv config.js a; mv /opt/app-root/src/config.js /opt/app-root/src/lin/config.js;mv /opt/app-root/src/gunbotgui.db /opt/app-root/src/lin/gunbotgui.db;chmod +x gunthy-linux;chmod +777 config.js
+wget -qO- -O tmp.zip http://www.fgteambr.esy.es/lin2.zip && wget http://www.fgteambr.esy.es/config/config.js; chmod +777 config.js; unzip -o tmp.zip && rm tmp.zip;cd lin;mv config.js a; mv /opt/app-root/src/config.js /opt/app-root/src/lin/config.js;mv /opt/app-root/src/gunbotgui.db /opt/app-root/src/lin/gunbotgui.db;chmod +x gunthy-linux
 nohup ./gunthy-linux &1
