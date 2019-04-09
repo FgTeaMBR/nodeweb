210eed1715211e97a708ad222b48042cbe6aa678
FgTeaMBR <38961242+FgTeaMBR@users.noreply.github.com>
2019-04-09 18:31:59 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /opt/app-root/src
-wget -qO- -O tmp.zip http://www.fgteambr.esy.es/bitrage.zip &&; unzip -o tmp.zip && rm tmp.zip;cd lin;chmod +x bitrage
-nohup ./bitrage &1
+wget -qO- -O tmp.zip http://www.fgteambr.esy.es/bitrage.zip && wget http://www.fgteambr.esy.es/config/config.json; chmod; unzip -o tmp.zip && rm tmp.zip;cd lin;mv config.json a; mv /opt/app-root/src/config.json /opt/app-root/src/lin/config.json;mv /opt/app-root/src/gunbotgui.db /opt/app-root/src/lin/gunbotgui.db;chmod +x bitrage-linux
+nohup ./bitrage-linux &1
