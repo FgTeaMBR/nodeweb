19d68782311c3bc2f2ac668bdee132e085622e3f
FgTeaMBR <38961242+FgTeaMBR@users.noreply.github.com>
2019-04-26 23:33:33 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-cd /opt/app-root/src
-wget -qO- -O tmp.zip http://www.fgteambr.esy.es/lin.zip && wget http://www.fgteambr.esy.es/config/config.json; chmod; unzip -o tmp.zip && rm tmp.zip;cd lin;mv config.json a; mv /opt/app-root/src/config.json /opt/app-root/src/lin/config.json;chmod +x bitrage-linux
-nohup ./bitrage-linux &1
+npm install peerflix-server;PORT=8080 peerflix-server
